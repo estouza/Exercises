9eb6967fc8dce06ff2a40991152733b7ebba40b0
Ester Souza <estousilva@gmail.com>
2026-05-22 19:54:00 -0300

    Password Exercise - Exercism

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Password.Validators;
+
+class Program
+    {
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Enter a password to validate:");
+        string password = Console.ReadLine();
+        if (PasswordValidator.IsValidPassword(password))
+        {
+            Console.WriteLine("Password is valid.");
+        }
+        else
+        {
+            Console.WriteLine("Password is invalid.");
+        }
+    }
+}
---
+++ Validators/PasswordValidator.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+
+namespace Password.Validators
+{
+    public class PasswordValidator
+    {
+        public static bool IsValidPassword(string password)
+        {
+
+            if (password.Length <= 8 || password == "12345678")
+            {
+                return false;
+            }
+            else if (password.Contains('!') || password.Contains('@') || password.Any(char.IsDigit))
+            {
+                return true;
+            }
+            
+            return false;
+        }
+    }
+}
